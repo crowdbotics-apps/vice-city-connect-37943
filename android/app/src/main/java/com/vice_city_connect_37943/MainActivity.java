6ce6ae40b41660799aa02b42330dfb7e212c7d18
Crowdbotics <team@crowdbotics.com>
2022-12-19 22:26:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vice_city_connect_37943/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vice_city_connect_37943;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "vice_city_connect_37943";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vice_city_connect_37943",
+  url: "https://vice_city_connect_37943.botics.co",
+  api: "https://vice_city_connect_37943.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
